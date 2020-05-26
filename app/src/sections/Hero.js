c1e304f441a550d20bfe845e5d95689f46d24cb7
Noemi <noemi01@csu.fullerton.edu>
2020-05-25 23:16:37 -0700

    Added the top and bottom pattern to the hero image

--- app/src/sections/Hero.js
+++ app/src/sections/Hero.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import {Container, Row, Col} from "react-bootstrap";
 import heroImg from '../assets/images/hero.gif'
+import bottomPattern from '../assets/images/bottompattern.svg';
+import topPattern from '../assets/images/toppattern.svg';
 
 function Hero() {
     return (
@@ -12,7 +14,9 @@ function Hero() {
                             <img src={heroImg} alt="Maya in her Graduation Gown"/>
                         </Col>
                         <Col className="hero-copy d-flex flex-column justify-content-center align-items-center px-3">
+                            <img className="top" src={bottomPattern} alt="top pattern"/>
                             <h1 className="display-2 text-white text-left mb-3">She believed<br/> she could,<br/> so she did.</h1>
+                            <img className="bottom" src={topPattern} alt="bottom pattern"/>
                         </Col>
                     </Row>
 
