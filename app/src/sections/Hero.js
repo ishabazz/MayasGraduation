73aba32f38dbe67cfe44fe4f7dd19a3cf6b888da
Noemi <noemi01@csu.fullerton.edu>
2020-05-25 19:12:22 -0700

    Removed some unnecessary classes

--- app/src/sections/Hero.js
+++ app/src/sections/Hero.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Jumbotron, Container, Row, Col} from "react-bootstrap";
+import {Container, Row, Col} from "react-bootstrap";
 import heroImg from '../assets/images/hero.gif'
 
 function Hero() {
