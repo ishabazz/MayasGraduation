3e0548521e808d976d32a300899e9c6ae770bf6f
Noemi <noemi01@csu.fullerton.edu>
2020-05-26 21:44:25 -0700

    Adjusted the video player

--- app/src/sections/Graduation.js
+++ app/src/sections/Graduation.js
@@ -3,22 +3,26 @@ import React from 'react';
 import {Row, Container, Col} from "react-bootstrap";
 import { Player } from 'video-react';
 import GraduationVideo from '../assets/images/graduation/graduation.mov'
+// import background from '../assets/images/graduation/graduation.svg';
 
 
 function Graduation() {
     return (
-        <Row className="section graduation">
+        <Row className="section graduation colorful-border">
             <Col>
                 <Container className="my-5">
-                    <h2 className="display-4 text-center">Watch me <span className="text-underline-danger">Graduate.</span></h2>
+                    <h2 className="display-4 text-center mb-5">Watch me <span className="text-underline-danger">graduate</span></h2>
                     <Player
                         fluid={false}
                         width={350}
-                        height={350}
+                        height={621.15}
                         src={GraduationVideo}
                     />
                     <div className="video-container">
-                        <p className="text-center">#mygraduation</p>
+                        <p className="text-success text-center">
+                            <span className="d-block font-weight-bold">Laguna Creek High School</span>
+                            <span className="d-block text-center">Class of 2020</span>
+                        </p>
                     </div>
                 </Container>
             </Col>
