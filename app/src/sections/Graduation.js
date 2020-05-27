f2f7a618ef4cf0d9e919e5ae9f2c4fb26c522222
Arturo Trejo <arturo.trejo10@gmail.com>
2020-05-26 20:22:00 -0700

    added graduation video

--- app/src/sections/Graduation.js
+++ app/src/sections/Graduation.js
@@ -1,6 +1,9 @@
 import React from 'react';
 
 import {Row, Container, Col} from "react-bootstrap";
+import { Player } from 'video-react';
+import GraduationVideo from '../assets/images/graduation/graduation.mov'
+
 
 function Graduation() {
     return (
@@ -8,6 +11,15 @@ function Graduation() {
             <Col>
                 <Container className="my-5">
                     <h2 className="display-4 text-center">Watch me <span className="text-underline-danger">Graduate.</span></h2>
+                    <Player
+                        fluid={false}
+                        width={350}
+                        height={350}
+                        src={GraduationVideo}
+                    />
+                    <div className="video-container">
+                        <p className="text-center">#mygraduation</p>
+                    </div>
                 </Container>
             </Col>
         </Row>
